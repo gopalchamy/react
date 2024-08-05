791b88ada68a72a0680e1f42ff09a06e86d8985e
gopalchamy <130646162+gopalchamy@users.noreply.github.com>
2024-08-05 11:15:30 +0530

    Add files via upload

---
+++ DAY 1/day1_CE3.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import './App.css';
+
+ const App=()=>{
+
+ }
+ const World=()=>{
+  return(
+    <div>
+      <p>This is World Functional Component</p>
+    </div>
+  )
+ }
+
+
+export default World;
